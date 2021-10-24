14683408ce03c679b38bdf117c9681fc9d63d06f
Bubbo2000 <elibu@DESKTOP-K5TNS6K>
2021-10-24 17:33:20 -0400

    challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,11 +13,33 @@ public Text score;
 
 private int scoreValue = 0;
 
+public Text wintext;
+
+public Text lives;
+
+private int livesValue = 3;
+
+public Text losetext;
+
+public AudioClip one;
+
+public AudioClip two;
+
+public AudioSource music;
+
 // Start is called before the first frame update
 void Start()
 {
 rd2d = GetComponent<Rigidbody2D>();
 score.text = scoreValue.ToString();
+lives.text = livesValue.ToString();
+music.clip = one;
+music.Play();
+music.loop = true;
+
+
+wintext.text = "";
+losetext.text = "";
 }
 
 // Update is called once per frame
@@ -35,9 +57,41 @@ if (collision.collider.tag == "Coin")
 scoreValue += 1;
 score.text = scoreValue.ToString();
 Destroy(collision.collider.gameObject);
+
+if (scoreValue == 4)
+{
+    transform.position = new Vector2(52.6f, 5f);
 }
+if (scoreValue == 8)
+    {
+        music.Stop();
+        music.clip = two;
+        music.Play();
+        
+    
+        Destroy(rd2d);
+        wintext.text = "You Win! Made By Elijah Bonnett";
+        
+    }
+
 
 }
+if (collision.collider.tag == "Enemy")
+{
+    livesValue -= 1;
+    lives.text = livesValue.ToString();
+    Destroy(collision.collider.gameObject);
+if (livesValue == 0)
+{
+    Destroy(rd2d);
+    losetext.text = "You Lose";
+}
+}
+
+
+}
+
+
 
 private void OnCollisionStay2D(Collision2D collision)
 {
@@ -49,4 +103,5 @@ rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of c
 }
 }
 }
+
 }
